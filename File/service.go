76cddba064a6e3a2984ef6a7c827b240966b0e22
yuualdary <wahyuyudistiro@gmail.com>
2021-10-08 01:30:38 +0700

    isprimaryupdate

--- File/service.go
+++ File/service.go
@@ -13,6 +13,8 @@ type Service interface {
 	CreateService(userID string, file string) (models.Filepdf, error)
 	UpdateFile(inputid DetailFile, file string) (models.Filepdf,error)
 	DetailFile(inputid DetailFile) (models.Filepdf,error)
+	ListAll(ID string)([]models.Filepdf,error)
+	UpdatePrimary(NewID string, UserID string)([]models.Filepdf,error)
 }
 
 
@@ -40,13 +42,23 @@ func (s *service)CreateService(userID string, file string) (models.Filepdf, erro
 	CreateFile.ID =  generatornumber.NewUUID()
 	CreateFile.Filename = CreateFile.ID+".pdf"
 	CreateFile.UserID = FindUserFromID.ID
-	
+	CreateFile.IsPrimary = true	
 	CreateUser, err := s.repository.CreateFile(CreateFile)
+
+	//Update Primary to unprimary
 
 	if err != nil {
 		
 		return CreateUser,err
 	}
+
+	_,err = s.UpdatePrimary(CreateFile.ID ,FindUserFromID.ID)
+
+	if err != nil {
+
+		return CreateUser,err
+	}
+
 
 	return CreateUser,nil
 
@@ -73,8 +85,12 @@ func (s *service)UpdateFile(inputid DetailFile, file string) (models.Filepdf,err
 
 		return models.Filepdf{},errors.New("Forbidden Access")
 		//TODO
+		//isprimary done
+		//check pdf dan ukuran file kalau bisa
+		//list dan validasi max 3x file
+		//update file set primary, delete file 
 		//buat validasi kalau yg access dari company tidak forbidden!
-		//buat hanya bisa 1 x cv atau bisa banyak upload tapi di set jadi primary (?)
+		//buat hanya bisa 1 x cv atau bisa banyak upload tapi di set jadi primary (?) done
 	}
 
 	FindFileID.Filename =FindFileID.ID+".pdf"
@@ -117,6 +133,50 @@ func (s *service)DetailFile(inputid DetailFile) (models.Filepdf,error){
 
 	return FindFileID,nil
 
+
+}
+
+func(s *service)ListAll(ID string)([]models.Filepdf,error){
+
+	GetList, err := s.repository.ListAllFile(ID)
+
+	if err != nil {
+
+		return GetList,err
+	}
+
+	return GetList,nil
+}
+
+
+func(s *service)UpdatePrimary(NewID string, UserID string)([]models.Filepdf,error){
+
+	GetList, err := s.repository.ListAllFile(UserID)
+
+	if err != nil {
+
+		return GetList,err
+	}
+
+	for _, ListFile := range GetList{
+
+		if ListFile.ID != NewID && ListFile.IsPrimary{
+
+			ListFile.IsPrimary = false
+			_,err := s.repository.UpdateFile(ListFile)
+			
+			if err != nil {
+
+				return []models.Filepdf{}, err
+			}
+
+
+		}
+
+
+	}
+
+	return GetList,nil
 
 }
 
